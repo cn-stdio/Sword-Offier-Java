8281505b023f9c1e93063a271452693c3a5782db
Seaguller <cn_stdio@163.com>
2019-11-14 15:15:31 +0800

    面试题08代码优化更新

--- 面试题08/src/Solution.java
+++ 面试题08/src/Solution.java
@@ -8,6 +8,13 @@
  * 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
  *
  * 实现思路：
+ * 首先分该节点有无右子树来讨论。
+ * 1. 若节点有右子树，则如果其右孩子不存在左子树，其右孩子就是要找的节点，而其右孩子存在左子树，就沿着其右孩子的左指针一路遍历下去，叶子节点就是我们要找的节点。
+ * 2. 若节点无右子树，这种情况就比较复杂。
+ * 2.1 首先要分是不是根节点，是根节点又没右子树直接返回null即可。
+ * 2.2 如果不是根节点的话，那还要看其本身是父节点的左孩子还是右孩子。
+ * 2.2.1 如果本身是父节点的左孩子，那直接返回父节点。
+ * 2.2.2 如果本身是父节点的右孩子，就沿着父节点遍历，直到找到一个节点，使得该节点是其父节点的左孩子，返回该节点的父节点即可。
  *
  * 牛客网测试用例：
  * https://www.nowcoder.com/practice/9023a0c988684a53960365b889ceaf5e?tpId=13&tqId=11210&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
@@ -45,33 +52,23 @@ public class Solution {
                 if(pNode.next.left == pNode) {
                     return pNode.next;
                 } else {
-                    /* 判断该节点的父节点的父节点是否存在 */
-                    if(pNode.next.next == null) {
-                        return null;
-                    } else {
-                        /* 根据左根右原则，遍历其父节点，直到找到为父节点左孩子的节点，返回该节点的父节点 */
-                        while(pNode.next.next!=null && pNode.next!=pNode.next.next.left) {
-                            pNode = pNode.next;
-                        }
-
-                        /* 这里不用判空，因为上述如果没有找到合乎条件的节点，pNode.next.next为null退出循环，自然返回null */
-                        return pNode.next.next;
-
+                    /* 根据左根右原则，遍历其父节点，直到找到为父节点左孩子的节点，返回该节点的父节点 */
+                    while(pNode.next.next!=null && pNode.next!=pNode.next.next.left) {
+                        pNode = pNode.next;
                     }
+
+                    /* 这里不用判空，因为上述如果没有找到合乎条件的节点，pNode.next.next为null退出循环，自然返回null */
+                    return pNode.next.next;
                 }
             }
         } else {
-            /* 判断右孩子有没有左子树 */
-            if(pNode.right.left == null) {
-                return pNode.right;
-            } else {
-                /* 有左子树，则沿着左孩子遍历下去，直到叶子节点就是下一个值 */
-                pNode = pNode.right;
-                while(pNode.left != null) {
-                    pNode = pNode.left;
-                }
-                return pNode;
+            /* 若没有左子树则不进入循环，直接返回原节点的右孩子 */
+            pNode = pNode.right;
+            /* 有左子树，则沿着左孩子遍历下去，直到叶子节点就是下一个值 */
+            while(pNode.left != null) {
+                pNode = pNode.left;
             }
+            return pNode;
         }
     }
 }
